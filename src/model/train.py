fe2aed41c5a0aead8f9fa17ac81823b83c63107b
SapnaCourse5i <sapna.verma@course5i.com>
2024-01-08 13:48:46 +0530

    3

--- src/model/train.py
+++ src/model/train.py
@@ -4,12 +4,9 @@ import glob
 import os
 import pandas as pd
 
-from sklearn.linear_model import LogisticRegression,train_test_split
+from sklearn.linear_model import LogisticRegression, train_test_split
 
 # define functions
-
-
-
 def split_data(df):
      X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
